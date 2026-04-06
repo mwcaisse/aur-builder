e92604ebe162ab718cf441cc1a72eaa3baa2b54b
Mitchell Caisse <mitchell@mwcaisse.com>
2026-04-05 20:23:29 -0400

    added tests for the field parser

--- src/package_parser.rs
+++ src/package_parser.rs
@@ -68,3 +68,88 @@ fn parse_fields_from_desc_file(contents: &str) -> Result<HashMap<String, Vec<Str
 
     Ok(fields)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::package_parser::parse_fields_from_desc_file;
+
+    const SIMPLE_DESC_CONTENTS: &str = "%FILENAME%
+bitwarden-bin-2026.3.1-1-x86_64.pkg.tar.zst
+
+%NAME%
+bitwarden-bin
+
+%VERSION%
+2026.3.1-1
+
+";
+
+    #[test]
+    fn test_parse_fields_from_desc_file_simple_fields() {
+        let results = parse_fields_from_desc_file(SIMPLE_DESC_CONTENTS);
+
+        assert!(results.is_ok());
+
+        let fields = results.unwrap();
+
+        assert_eq!(fields.len(), 3);
+
+        assert!(fields.contains_key("NAME"));
+        assert!(fields.contains_key("FILENAME"));
+        assert!(fields.contains_key("VERSION"));
+
+        assert_eq!(fields.get("NAME").unwrap().len(), 1);
+        assert_eq!(fields.get("FILENAME").unwrap().len(), 1);
+        assert_eq!(fields.get("VERSION").unwrap().len(), 1);
+
+        assert_eq!(fields.get("NAME").unwrap()[0], "bitwarden-bin");
+        assert_eq!(
+            fields.get("FILENAME").unwrap()[0],
+            "bitwarden-bin-2026.3.1-1-x86_64.pkg.tar.zst"
+        );
+        assert_eq!(fields.get("VERSION").unwrap()[0], "2026.3.1-1");
+    }
+
+    const MULTILINE_DESC_CONTENTS: &str = "%DEPENDS%
+aspnet-runtime-6.0
+gcc-libs
+glibc
+sqlite
+
+%MAKEDEPENDS%
+dotnet-sdk-6.0
+yarn";
+
+    #[test]
+    fn test_parse_fields_from_desc_files_multiline_values() {
+        let results = parse_fields_from_desc_file(MULTILINE_DESC_CONTENTS);
+        assert!(results.is_ok());
+
+        let fields = results.unwrap();
+        assert_eq!(fields.len(), 2);
+
+        assert!(fields.contains_key("DEPENDS"));
+        assert!(fields.contains_key("MAKEDEPENDS"));
+
+        assert_eq!(fields.get("DEPENDS").unwrap().len(), 4);
+        assert_eq!(fields.get("MAKEDEPENDS").unwrap().len(), 2);
+
+        assert_eq!(
+            fields.get("DEPENDS").unwrap(),
+            &vec!["aspnet-runtime-6.0", "gcc-libs", "glibc", "sqlite"]
+        );
+        assert_eq!(
+            fields.get("MAKEDEPENDS").unwrap(),
+            &vec!["dotnet-sdk-6.0", "yarn"]
+        );
+    }
+
+    const ERROR_VALUE_WITHOUT_FIELD_NAME_CONTENT: &str = "HELLO
+%MAKEDEPENDS%";
+
+    #[test]
+    fn test_parse_fields_from_desc_files_no_values() {
+        let results = parse_fields_from_desc_file(ERROR_VALUE_WITHOUT_FIELD_NAME_CONTENT);
+        assert!(!results.is_ok());
+    }
+}
