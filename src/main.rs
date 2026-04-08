4dd5288a16a0a03e311fe04da25514704e105197
Mitchell Caisse <mitchell@mwcaisse.com>
2026-04-07 21:40:01 -0400

    look for default config in opt dir

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 use std::process::exit;
 
 fn main() {
-    let default_config_path: String = "/etc/aur-builder/config.yaml".to_string();
+    let default_config_path: String = "/opt/aur-builder/config.yaml".to_string();
 
     let matches = command!()
         .about("A helper to create local AUR repos and manage the packages inside of them.")
